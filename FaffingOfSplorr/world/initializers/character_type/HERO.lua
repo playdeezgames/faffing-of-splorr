ad563323bac764d1616a88285d0e7f77a0b462a7
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-01-30 05:05:55 -0600

    i cant move into trees!

--- FaffingOfSplorr/world/initializers/character_type/HERO.lua
+++ FaffingOfSplorr/world/initializers/character_type/HERO.lua
@@ -4,13 +4,22 @@ local character = require "world.character"
 local room_cell = require "world.room_cell"
 local room = require "world.room"
 local directions = require "game.directions"
+local room_cell_type = require "world.room_cell_type"
+
+local function can_enter(room_cell_id)
+    if room_cell_id == nil then return false end
+    local room_cell_type_id = room_cell.get_room_cell_type(room_cell_id)
+    if room_cell_type.get_blocking(room_cell_type_id) then return false end
+    return true
+end
+
 local function do_move(character_id, direction_id)
     local room_cell_id = character.get_room_cell(character_id)
     local column, row = room_cell.get_position(room_cell_id)
     local next_column, next_row = directions.get_next_position(direction_id, column, row)
     local room_id = room_cell.get_room(room_cell_id)
     local next_room_cell_id = room.get_room_cell(room_id, next_column, next_row)
-    if next_room_cell_id ~= nil then
+    if can_enter(next_room_cell_id) then
         character.set_room_cell(character_id, next_room_cell_id)
     end
 end
