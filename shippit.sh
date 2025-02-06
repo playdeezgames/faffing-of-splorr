44dfdb34d65f26913ce2ae82dc0b822bfdc0e2a9
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-02-06 04:36:01 -0600

    i've got a shippit script

--- shippit.sh
+++ shippit.sh
@@ -1,1 +1,7 @@
+rm -f faffing.zip
+rm -f faffing.love
+rm -f pub/faffing.exe
 7z a -tzip -r faffing.love @listfile.txt
+cat pub/love.exe faffing.love > pub/faffing.exe
+7z a -tzip faffing.zip @listfile2.txt
+butler push faffing.zip thegrumpygamedev/tree-punchers-of-splorr:windows
